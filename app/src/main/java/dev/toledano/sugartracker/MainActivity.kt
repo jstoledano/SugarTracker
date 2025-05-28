e741327804ccf08ec4cc49b2efc19a7aabc2d63e
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2025-05-27 23:47:59 -0600

    Inicializa repositorio local con README y .gitignore

---
+++ app/src/main/java/dev/toledano/sugartracker/MainActivity.kt
@@ -0,0 +1,18 @@
+package dev.toledano.sugartracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Text("Hello SugarTracker")
+            }
+        }
+    }
+}
